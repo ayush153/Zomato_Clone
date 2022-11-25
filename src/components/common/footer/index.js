8c4d3bf225950a45bde50106f9e5e75b854e4a92
Ayush kumar <jhaaayushmarch@gmail.com>
2022-11-25 18:09:15 +0530

    Add files via upload

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made with 🔥 by Ayush,Copyright &copy; 2022</div>
+    
+  );
+};
+
+export default Footer;
